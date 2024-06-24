035103744dcc38e4ef7caec6272c8e0f3595b389
rooty <gutnikov.mihail@mail.ru>
2024-06-24 20:04:57 +0300

    stepik непонял 2.1 5упр

--- proba.py
+++ proba.py
@@ -1,1 +1,8 @@
-print(";elgrn;wge")
+a, b = map(int, input().split())
+if a >= 30 and b >= 30:
+    print(f"Можно отрезать {a // 30 * b // 30} квадратов.")
+else:
+    print("Можно отрезать 0 квадратов.")
+print(a // 30)
+print(b // 30)
+print(a // 30 * b // 30)
