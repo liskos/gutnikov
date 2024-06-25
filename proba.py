7deae7d26afa0b9f718d6d71946a912609301b1e
kos <liskosnik@yandex.ru>
2024-06-25 18:38:50 +0300

    25/06/2024

--- proba.py
+++ proba.py
@@ -1,8 +1,2 @@
-a, b = map(int, input().split())
-if a >= 30 and b >= 30:
-    print(f"Можно отрезать {a // 30 * b // 30} квадратов.")
-else:
-    print("Можно отрезать 0 квадратов.")
-print(a // 30)
-print(b // 30)
-print(a // 30 * b // 30)
+print(4 & 5)
+print(4 and 5)
