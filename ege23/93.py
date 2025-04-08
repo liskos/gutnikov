ca3301c3d2bd65f85c203b9f201323e1a10da0aa
rooty <gutnikov.mihail@mail.ru>
2025-04-08 16:23:33 +0300

    8\04\25

---
+++ ege23/93.py
@@ -0,0 +1,10 @@
+def f(a,b):
+    if a == b:
+        return 1
+    if a < b:
+        return 0
+    if a == 10:
+        return 0
+    return f(a - 1,b) + f(a - 5, b) + f(a // 3, b)
+
+print(f(22, 2))
